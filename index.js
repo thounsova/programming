6fe497d27d48061b0a06804c85aaae63ba3ddc82
reach <thoun.sovannareach@institute.ngo>
2024-12-20 16:05:58 +0700

    add

--- index.js
+++ index.js
@@ -4,31 +4,66 @@
 // Task 1: Comment Your JavaScript Code
 // Write a single-line comment below this line:
 
+let a=4;//Thoun Sovnnareach,I am a stundent:me study at pse //
+
+
 // Task 2: Declare JavaScript Variables
 // Declare a variable named `myName`:
+var myName;
+
 
 // Task 3: Storing Values with the Assignment Operator
 // Assign the value "JavaScript" to a variable named `myLanguage`:
 
+var myLanguage = 5;
+
 // Task 4: Assigning the Value of One Variable to Another
 // Declare a variable named `a` with the value 5, and another variable `b`. Assign `a` to `b`:
+
+var a = 5;
+    
+var b = a;
 
 // Task 5: Initializing Variables with the Assignment Operator
 // Declare and initialize a variable named `myAge` with the value 25:
 
+var myAge;
+myAge =25;
 // Task 6: Declare String Variables
 // Declare a string variable named `greeting` and set it to "Hello, World!":
+let greeting = "Hello, World!";
 
+console.log(greeting)
 // Task 7: Understanding Uninitialized Variables
 // Declare three variables `x`, `y`, and `z` without assigning values. Then, assign values 1, 2, and 3 to them:
+var x;
+var y;
+var z;
+
+x = x + 1;
+y = y + 2;
+z = z = 3;
 
 // Task 8: Understanding Case Sensitivity in Variables
 // Fix the variable names to follow case sensitivity:
 // let StUdEnTnAmE = "John";
 // let sTuDeNtAgE = 20;
+let studentName ="john"
+let stundentAge =20;
 
 // Task 9: Explore Differences Between the var and let Keywords
 // Declare a variable using `var` and another using `let`. Show the difference in scope (use a block of code to demonstrate):
+// var catName = "Oliver";
+// let catSound = "Meow!";
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+
 
 // Task 10: Declare a Read-Only Variable with the const Keyword
 // Declare a constant variable `PI` and assign it the value 3.14:
+var fCC = "freeCodeCamp"; // Change this line
+var fact = 3.14; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact);
